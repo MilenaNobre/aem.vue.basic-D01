1d69308d6409c56244c4682c7346857cfd85ae2b
Milena Nobre <mnmbrito@hotmail.com>
2022-05-03 12:37:00 -0300

    Ajuste para mostrar a logo na tela de login-mobile

--- ui.frontend/src/components/CustomClasses/CustomClasses.vue
+++ ui.frontend/src/components/CustomClasses/CustomClasses.vue
@@ -11,6 +11,12 @@ export default {
 
     const containerLeft = document.querySelector('[cqpath="/content/vue/vue/login/jcr:content/root/responsivegrid/container"] div')
     containerLeft.classList.add('container-center')
+
+    const containerImgLogoMobile = document.querySelector('[cqpath="/content/vue/vue/login/jcr:content/root/responsivegrid/image"] ')
+    containerImgLogoMobile.classList.add('img-mobile-logo')
+
+    const ImgLogoMobile = document.querySelector('[cqpath="/content/vue/vue/login/jcr:content/root/responsivegrid/image"] img')
+    ImgLogoMobile.classList.add('img-logo-hidden')
   }
 }
 </script>
